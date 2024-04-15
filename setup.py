9ae3d3aea94a095830fb467bdcd5d57187350ebe
voidful <voidful.stack@gmail.com>
2024-04-15 18:32:49 +0800

    chore(setup): bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='fta',
-    version='0.0.6',
+    version='0.0.7',
     description='',
     url='https://github.com/voidful/FTA',
     author='Voidful',
